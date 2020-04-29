6f0082dc3accf2c5e9098cd07776fe154ba43ae5
bolcof <sanctum.crusade.ex@gmail.com>
2020-04-29 20:25:35 +0900

    戻し

---
+++ Assets/Script/Obj/ObjUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjUnit : MonoBehaviour
+{
+    public int Slot;
+    public GameObject[] ObjRoots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Spawn(GameObject Obj, int posID)
+    {
+        Instantiate(Obj, ObjRoots[posID].transform.position, Quaternion.identity, ObjRoots[posID].transform);
+    }
+}
